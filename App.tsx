aa97557d351b5d5b668256af5d410fd08075e35b
a <a>
2024-02-21 18:32:58 +0530

    added carousel and add the button at top,2 buttons in buttom and also add them functionality

--- App.tsx
+++ App.tsx
@@ -1,54 +1,145 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useEffect, useRef, useState} from 'react';
 
-import React, {useEffect} from 'react';
-import type {PropsWithChildren} from 'react';
 import SplashScreen from 'react-native-splash-screen';
+
 import {
-  Platform,
+  Button,
+  Dimensions,
+  Image,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+import {IMAGES} from './src/assets/images';
 
 function App(): React.JSX.Element {
+  const [activeDotIndex, setActiveDotIndex] = useState(0);
+
+  const _carouselRef = useRef();
+
   useEffect(() => {
     SplashScreen.hide();
   }, []);
-  return (
-    <SafeAreaView>
+  const data = [
+    {
+      id: 1,
+      title: 'Browse Product',
+      image: IMAGES.ONE,
+      descriptio:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    },
+    {
+      id: 2,
+      title: 'Play Seemlessly',
+      image: IMAGES.TWO,
+      descriptio:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    },
+    {
+      id: 3,
+      title: 'Track Order',
+      image: IMAGES.THREE,
+      descriptio:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    },
+  ];
+
+  const _renderItem = ({item, index}: {item: {}; index: number}) => {
+    return (
       <View>
-        <Text>sameer</Text>
+        <Image
+          source={
+            //@ts-ignore
+            item.image
+          }
+          style={{
+            width: Dimensions.get('screen').width,
+            height: Dimensions.get('screen').width,
+          }}
+        />
+        <View style={{padding: 15}}>
+          <Text style={{fontSize: 18, fontWeight: '700'}}>
+            {
+              //@ts-ignore
+              item.title
+            }
+          </Text>
+          <Text style={{fontSize: 16, fontWeight: '500'}}>
+            {
+              //@ts-ignore
+              item.descriptio
+            }
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1, justifyContent: 'space-betwceen'}}>
+      <View style={{alignItems: 'flex-end', padding: 15}}>
+        <TouchableOpacity>
+          <Text style={{color: 'black'}}>Skip</Text>
+        </TouchableOpacity>
+      </View>
+      <Carousel
+        ref={_carouselRef}
+        data={data}
+        renderItem={_renderItem}
+        sliderWidth={Dimensions.get('window').width}
+        itemWidth={Dimensions.get('window').width}
+        onSnapToItem={index => setActiveDotIndex(index)}
+      />
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Pagination
+          dotsLength={3}
+          activeDotIndex={activeDotIndex}
+          carouselRef={_carouselRef}
+          dotStyle={{
+            width: 15,
+            backgroundColor: 'teal',
+          }}
+          inactiveDotStyle={{
+            width: 10,
+          }}
+        />
+        <View style={{padding: 15, flexDirection: 'row', gap: 10}}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              //@ts-ignore
+              _carouselRef.current.snapToItem(activeDotIndex - 1);
+            }}>
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                backgroundColor: 'lightgray',
+              }}
+            />
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              //@ts-ignore
+              _carouselRef.current.snapToItem(activeDotIndex + 1);
+            }}>
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                backgroundColor: 'teal',
+              }}
+            />
+          </TouchableWithoutFeedback>
+        </View>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const IMAGES = {
+  ONE: require('./one.png'),
+  TWO: require('./two.png'),
+  THREE: require('./three.png'),
+};
+
+export {IMAGES};
